96dde7a630d18986ebd36c78e3c1886f06fbce33
Ali-Elouasbi <ali.elouasbi@mths.ca>
2024-11-24 20:26:10 -0500

    2-13

--- main.ts
+++ main.ts
@@ -1,8 +1,50 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ali Elouasbi
+ * Created on: Nov 2024
+ * This program...
 */
 
-basic.showString('Hello, World!')
+//variables
+let strip = robotbit.rgb()
+
+//setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(0)
+    strip.clear()
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    strip.show()
+    basic.pause(1000)
+    
+    basic.showNumber(1)
+    strip.clear()
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    strip.show()
+    basic.pause(1000)
+   
+    basic.showNumber(2)
+    strip.clear()
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+    strip.show()
+    basic.pause(1000)
+    
+    basic.showNumber(3)
+    strip.clear()
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    strip.show()
+    basic.pause(1000)
+    
+    basic.showNumber(4)
+    strip.clear()
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+    strip.show()
+})
